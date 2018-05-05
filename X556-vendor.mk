827587f7e1a5995b0221fefa94613a5b2c425ef5
nasreirma <nasreirma@gmail.com>
2018-05-05 15:14:03 +0000

    updating blobs

--- X556-vendor.mk
+++ X556-vendor.mk
@@ -1,3 +1,5 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/bin,system/bin)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor/bin,system/vendor/bin)
